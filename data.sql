b973e2753e0a5be72a04543497d923f5aec1e463
ayubch <ayoub.chahir96@gmail.com>
2022-04-27 12:20:01 +0000

    Add Solution

--- data.sql
+++ data.sql
@@ -1,6 +1,15 @@
-/* Populate database with sample data. */
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Agumon',"03/02/2020",10.23,0,"1");
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Gabumon',"15/11/2018",8,2,"1");
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Pikachu',"7/1/2021,",15.04,"0");
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Devimon',"7/1/2021,",15.04,"1");
+/* Populate database with aniamls data. */
 
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 0, false, -11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', '2021-11-15', 2, true, -5.7);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-04-02', 3, false, -12.13);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-06-12', 1, true, -45);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-05-14', 4, true, 22);
