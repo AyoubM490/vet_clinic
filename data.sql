3972f1c2f69bd8f58f2acdf08a0a4d24dccb018f
ayubch <ayoub.chahir96@gmail.com>
2022-05-30 16:36:15 +0100

    Query and Update animals table

--- data.sql
+++ data.sql
@@ -309,4 +309,4 @@ VALUES
 	( (SELECT id from animals where name = 'Blossom'), (SELECT id from vets where name = 'Stephanie Mendez'), '2020-05-24' ),
 	( (SELECT id from animals where name = 'Blossom'), (SELECT id from vets where name = 'William Tatcher'), '2021-01-11' );
 
-  COMMIT;
+  
