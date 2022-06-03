057e92245cd4858070e9d6d7a749b3d9fb835a2d
ayubch <ayoub.chahir96@gmail.com>
2022-06-03 08:54:16 +0100

    Modify date_of_graduation for Code Review

--- data.sql
+++ data.sql
@@ -426,8 +426,8 @@ VALUES
   COMMIT;
 
 INSERT INTO vets (name, age, date_of_graduation) VALUES('William Tatcher', 45, '2000-04-23');
-INSERT INTO vets (name, age, date_of_graduation) VALUES('Maisy Smith', 26, '2000-01-17');
-INSERT INTO vets (name, age, date_of_graduation) VALUES('Stephanie Mendez', 64, '2000-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES('Stephanie Mendez', 64, '1981-05-04');
 INSERT INTO vets (name, age, date_of_graduation) VALUES('Jack Harkness', 38, '2008-06-08');
 
 INSERT INTO specializations (vet_id, species_id) 
